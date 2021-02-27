83dff18972b325415413a3c41b8e4939060d0153
mischahaenen <mischa.j.haenen@gmail.com>
2021-02-27 16:07:39 +0100

    update to angular v11

--- src/app/home/article-list/article-filter/article-filter.component.spec.ts
+++ src/app/home/article-list/article-filter/article-filter.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ArticleFilterComponent } from './article-filter.component';
 
@@ -6,7 +6,7 @@ describe('ArticleFilterComponent', () => {
   let component: ArticleFilterComponent;
   let fixture: ComponentFixture<ArticleFilterComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ArticleFilterComponent ]
     })
--- src/app/home/article-list/next-events/next-events.component.spec.ts
+++ src/app/home/article-list/next-events/next-events.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { NextEventsComponent } from './next-events.component';
 
@@ -6,7 +6,7 @@ describe('NextEventsComponent', () => {
   let component: NextEventsComponent;
   let fixture: ComponentFixture<NextEventsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ NextEventsComponent ]
     })
