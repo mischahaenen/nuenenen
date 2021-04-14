c0f3179e8f7cb5a44784228828c8194d8a260099
mischahaenen <mischa.j.haenen@gmail.com>
2021-04-14 11:52:36 +0200

    Move to structure code better

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
-import { LoginComponent } from './header/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './components/login/login.component';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from './modules/material.module';
 
 @NgModule({
   declarations: [HeaderComponent, LoginComponent],
   imports: [CommonModule, RouterModule, MaterialModule],
-  exports: [HeaderComponent]
+  exports: [HeaderComponent],
 })
 export class CoreModule {}
